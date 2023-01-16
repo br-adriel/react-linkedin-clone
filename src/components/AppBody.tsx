37195754a317c5b77b0d95584e03135705f91a61
Adriel <adriel.fsantos@outlook.com>
2023-01-16 20:46:05 -0300

    Adiciona componente AppBody

---
+++ src/components/AppBody.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Container1128px } from './Container';
+
+const AppBody = styled(Container1128px)`
+  display: flex;
+  justify-content: center;
+  align-items: left;
+  margin-top: 24px;
+`;
+
+export default AppBody;
