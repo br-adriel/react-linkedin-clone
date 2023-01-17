1feb10c5142d1436ee2aaecb7ac7ade66a33b3bf
Adriel <adriel.fsantos@outlook.com>
2023-01-16 22:27:24 -0300

    Altera propriedade flex de Sidebar

--- src/components/Sidebar/Sidebar.style.tsx
+++ src/components/Sidebar/Sidebar.style.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.aside`
   height: 100%;
-  flex: 0.22;
+  flex: 0.2;
   display: flex;
   flex-direction: column;
   gap: 8px;
