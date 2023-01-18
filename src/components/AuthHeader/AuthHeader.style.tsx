00d0d71723977c06d131667990f04bc439325c25
Adriel <adriel.fsantos@outlook.com>
2023-01-18 18:01:11 -0300

    Adiciona componente AuthHeader #10 #11

---
+++ src/components/AuthHeader/AuthHeader.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: left;
+  width: 100%;
+
+  svg {
+    margin-top: 32px;
+    margin-left: 56px;
+  }
+`;
