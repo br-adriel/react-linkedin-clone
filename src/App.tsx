1cb84b5d69a8a8f8606483df5d05034e991310dd
Adriel <adriel.fsantos@outlook.com>
2023-01-18 18:02:45 -0300

    Implementa rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-import AppBody from './components/AppBody';
-import Feed from './components/Feed';
+import { Outlet } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import { RootState } from './store';
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <AppBody>
-        <Sidebar />
-        <Feed />
-      </AppBody>
+      <Outlet />
     </>
   );
 }
