443122f7b85f3f35bdb58aff92681be804fe4fd8
Adriel <adriel.fsantos@outlook.com>
2023-01-20 16:40:15 -0300

    Implementa persistencia de login #10

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
+import { auth } from './services/firebase';
+import { login, logout } from './store/user/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth)
+        dispatch(
+          login({
+            email: userAuth.email,
+            displayName: userAuth.displayName,
+            uid: userAuth.uid,
+            photoURL: userAuth.photoURL,
+          } as User)
+        );
+      else dispatch(logout());
+    });
+  }, []);
   return (
     <>
       <GlobalStyle />
